d5842a87cdc68037b06f4d1abc2a08d773587db9
PioDam <82443691+PioDam@users.noreply.github.com>
2023-11-15 14:13:40 +0100

    Dodaj pliki projektów.

---
+++ bajki/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bajki.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Route("/admin")]
+        public IActionResult AdminLogin()
+        {
+            return View();
+        }
+    }
+}
